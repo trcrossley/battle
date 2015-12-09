7660fcec2626e0263bfde452ac3418ade0d110d6
Tristan Crossley <trcrossley@gmail.com>
2015-12-09 12:24:23 +0000

    CH24: Refactored Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,18 @@
 class Player
 
+  DEFAULT_HP = 60
   attr_reader :name, :hp
-  attr_writer :hp
 
-  def initialize(name)
+  def initialize(name, hp = DEFAULT_HP)
     @name = name
-    @hp = 60
+    @hp = hp
   end
 
   def attack(player)
-    player.hp -= 10
-    "#{@name} attacks #{player.name} for 10 damage!"
+    player.damage
   end
 
+  def damage
+    @hp -= 10
+  end
 end
